15c35c223f14c917f16e29a258a0eeb748684b0e
SamNgigi <sam.t.ngigi@gmail.com>
2023-12-05 16:49:25 +0300

    [feat]: Adds persisting state by using rememberSaveable in MyApp Composable

--- app/src/main/java/com/example/composebasics/MainActivity.kt
+++ app/src/main/java/com/example/composebasics/MainActivity.kt
@@ -4,10 +4,13 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -20,9 +23,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,7 +48,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(modifier: Modifier = Modifier){
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
     Surface(modifier){
         if(shouldShowOnboarding){
             OnboardingScreen(onContinueClicked = {shouldShowOnboarding = false})
@@ -75,6 +80,7 @@ fun OnboardingScreen(
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun MyGreetings(
     modifier: Modifier = Modifier,
@@ -82,6 +88,17 @@ private fun MyGreetings(
 ){
     Surface(modifier = modifier, color = MaterialTheme.colorScheme.background){
         LazyColumn(modifier = modifier.padding(vertical = 4.dp)){
+            stickyHeader {
+                Column(modifier = modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+                    .background(color = Color.Black)){
+                    Text(
+                        text = "Sample Header",
+                        modifier = modifier.fillMaxWidth()
+                    )
+                }
+            }
             items(items = names){name ->
                 Greeting(name = name)
             }
